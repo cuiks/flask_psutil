160ce2416db069770809a0974e7fe7f492b89711
Mr-cuiks <1253016652@qq.com>
2018-09-27 19:07:37 +0800

    master

--- save_status.py
+++ save_status.py
@@ -11,15 +11,10 @@ sql = '''insert into cpu_used(cpu_used,add_time) values (%s,%s)'''
 
 def save_cpu():
     while True:
-        # print('执行')
         add_time = int(time.time() * 1000)
         cpu_used = psutil.cpu_percent(interval=None)
-        # print(add_time, cpu_used)
         time.sleep(1)
         sql_now = sql % (cpu_used, add_time)
         cursor.execute(sql_now)
         conn.commit()
 
-
-# th_saveCpu = threading.Thread(target=save_cpu)
-# th_saveCpu.start()
